3c4f6a5ad57a2c540cbfeab4ab25612a8efb2285
Alejandro Pascual <ale14.2012@gmail.com>
2025-11-10 00:16:05 -0300

    Modificacion a carpeta Controllers, a los archivos mail.controller y prestamos.controller

--- Trabajo Grupales/Grupo N° 6 - TP 6/src/config/prisma.js
+++ Trabajo Grupales/Grupo N° 6 - TP 6/src/config/prisma.js
@@ -1,3 +1,16 @@
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
-export default prisma;
+// src/config/prisma.js
+const { PrismaClient } = require('@prisma/client');
+
+let prisma;
+
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient();
+} else {
+  // evita múltiples conexiones en dev con nodemon
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
+
+module.exports = prisma;
--- Trabajo Grupales/Grupo N° 6 - TP 6/src/controllers/mail.controller.js
+++ Trabajo Grupales/Grupo N° 6 - TP 6/src/controllers/mail.controller.js
@@ -1,14 +1,39 @@
+// const { enviarEmail } = require("../services/email.service");
+
+// // Controlador de prueba para enviar un email
+// const testMailController = async (req, res) => {
+//   // recuperamos el mail del body
+//   const { mail } = req.body;
+
+//   // enviamos el email de prueba
+//   await enviarEmail(mail);
+
+//   return res.status(200).json({ message: "Email enviado correctamente" });
+// };
+
+// module.exports = {
+//   testMailController,
+// };
+
+
 const { enviarEmail } = require("../services/email.service");
 
 // Controlador de prueba para enviar un email
 const testMailController = async (req, res) => {
-  // recuperamos el mail del body
-  const { mail } = req.body;
+  try {
+    const { mail } = req.body;
 
-  // enviamos el email de prueba
-  await enviarEmail(mail);
+    if (!mail) {
+      return res.status(400).json({ message: "El campo 'mail' es obligatorio" });
+    }
 
-  return res.status(200).json({ message: "Email enviado correctamente" });
+    await enviarEmail(mail);
+
+    return res.status(200).json({ message: "Email enviado correctamente" });
+  } catch (error) {
+    console.error("Error al enviar el email:", error);
+    return res.status(500).json({ message: "Error al enviar el email", error });
+  }
 };
 
 module.exports = {
