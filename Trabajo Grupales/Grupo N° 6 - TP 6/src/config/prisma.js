fe029e029d46754d33e5d882b0ff04e8ad5dd67c
Florencia Diaz <florenciamagalidiaz15@gmail.com>
2025-11-14 09:48:21 -0300

    Resuelvo conflictos del merge

--- Trabajo Grupales/Grupo N° 6 - TP 6/src/config/prisma.js
+++ Trabajo Grupales/Grupo N° 6 - TP 6/src/config/prisma.js
@@ -1,6 +1,4 @@
-<<<<<<< HEAD
 
-// src/config/prisma.js
 const { PrismaClient } = require('@prisma/client');
 
 let prisma;
@@ -8,7 +6,6 @@ let prisma;
 if (process.env.NODE_ENV === 'production') {
   prisma = new PrismaClient();
 } else {
-  // evita múltiples conexiones en dev con nodemon
   if (!global.prisma) {
     global.prisma = new PrismaClient();
   }
@@ -17,23 +14,3 @@ if (process.env.NODE_ENV === 'production') {
 
 module.exports = prisma;
 
-=======
-// const {PrismaClient} = require ('@prisma/client')
-
-// const prisma = new PrismaClient();
-// module.exports={prisma};
-
-
-
-
-
-// src/config/prisma.js
-import { PrismaClient } from '@prisma/client';
-
-const globalForPrisma = globalThis;
-const prisma = globalForPrisma.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
-
-export default prisma;
->>>>>>> SofiaJimenez-61256-TP3
