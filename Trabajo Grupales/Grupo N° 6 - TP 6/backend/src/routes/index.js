ea0e004e2a39c80557c47ec8850aa1724445f925
Florencia Diaz <florenciamagalidiaz15@gmail.com>
2025-11-16 22:25:20 -0300

    Actualizacion

--- Trabajo Grupales/Grupo N° 6 - TP 6/backend/src/routes/index.js
+++ Trabajo Grupales/Grupo N° 6 - TP 6/backend/src/routes/index.js
@@ -6,5 +6,10 @@ router.get('/health', (req, res) => res.json({ ok: true }));
 
 // Monta todas las rutas de autenticación
 router.use('/auth', require('./auth.routes'));
+router.use('/alumnos', require('./alumnos'));
+router.use('/libros', require('./libros'))
+router.use('/prestamos',require('./prestamos'))
+router.use('/mail',require('./mail.routes'))
+
 
 module.exports = router;
